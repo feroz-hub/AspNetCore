86180af3f8d93448199eec6123ca87186d9e5714
Feroze basha S <bashaferoz66@gmail.com>
2024-11-11 12:46:15 +0530

    MiddlewareExample

--- 03. Middleware/01. Run/MiddlewareExample/Program.cs
+++ 03. Middleware/01. Run/MiddlewareExample/Program.cs
@@ -3,4 +3,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello");
+});
+/*The Extension method called "Run" is used to execute a terminating/short-circuiting middleware
+that doesn't forward the request to the next middleware*/ 
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello again");
+});
+
 app.Run();
