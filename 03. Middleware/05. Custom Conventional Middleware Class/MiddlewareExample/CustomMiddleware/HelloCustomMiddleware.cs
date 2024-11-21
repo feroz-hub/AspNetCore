a553dc82dd2b0e25481d4b19bee71662b6c8276a
Feroze basha S <bashaferoz66@gmail.com>
2024-11-21 11:49:54 +0530

    Custom Conventiona Middleware

---
+++ 03. Middleware/05. Custom Conventional Middleware Class/MiddlewareExample/CustomMiddleware/HelloCustomMiddleware.cs
@@ -0,0 +1,23 @@
+namespace MiddlewareExample.CustomMiddleware;
+
+public class HelloCustomMiddleware(RequestDelegate next)
+{
+    public async Task Invoke(HttpContext httpContext)
+    {
+        if (httpContext.Request.Query.ContainsKey("firstname") && httpContext.Request.Query.ContainsKey("lastname"))
+        {
+            string fullname = httpContext.Request.Query["firstname"] + httpContext.Request.Query["lastname"];
+            await httpContext.Response.WriteAsync($"fullname:\n"+fullname);
+        }
+
+        await next(httpContext);
+    }
+}
+
+public static class HelloCustomMiddlewareExtension
+{
+    public static IApplicationBuilder UseHelloCustomMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<HelloCustomMiddleware>();
+    }
+}
